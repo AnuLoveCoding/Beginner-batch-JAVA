2b69507d779a40ccfda952b07eecea96599f761a
anuLoveCoading <anusrivas07@gmail.com>
2022-09-22 22:48:04 +0530

    class 02

--- prblm1.java
+++ prblm1.java
@@ -4,12 +4,14 @@
 public class prblm1 {
 
     public static void main(String[] args) {
-        int a = 40;
-        int b= 10;
+        
+        int a = 5;
+        int b= 7;
 
-        System.out.println(a+b);
+        // System.out.println(a+b);
         System.out.println("sum of  "+ a + " and "+ b + " is = " + (a+b));
-        System.out.println("Anurag");
+        System.out.println("Multi of  "+ a + " and "+ b + " is = " + (a*b));
+        // System.out.println("Anurag");
 
     }
 }
