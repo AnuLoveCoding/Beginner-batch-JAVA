61d8745e3ed15ea7d3ab8cb2944412fdaed2ad4f
anuLoveCoading <anusrivas07@gmail.com>
2022-09-22 23:41:22 +0530

    class 03

---
+++ prblm2.java
@@ -0,0 +1,16 @@
+public class prblm2 {
+    public static void main(String[] args) {
+        System.out.println("125"+ 5);
+        System.out.println(5+"125"+ 5);
+        System.out.println(5*5+"125"+ 5);
+        // System.out.println("125"+ 5);
+        // System.out.println("125"+ 5);
+        // System.out.println("125"+ 5);
+        // System.out.println("125"+ 5);
+        // System.out.println("125"+ 5);
+        // System.out.println("125"+ 5);
+        // System.out.println("125"+ 5);
+        // System.out.println("125"+ 5);
+        // System.out.println("125"+ 5);
+    }
+}
---
+++ prblm3.java
@@ -0,0 +1,6 @@
+public class prblm3 {
+    public static void main(String[] args) {
+        System.out.println("Computers are commonly used in many feilds in our present world as will be in the future. These machines have proven to be very essential and significant epcially for people who run large organization and industries. But also to the individuals.A computer can complete the work of 10 people or  more in a function of seconds. We can type our documents and either other ");
+        System.out.println("");
+    }
+}
