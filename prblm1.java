497a6c8ed3e18e2ef1718be8e05f088e536078b3
anuLoveCoading <anusrivas07@gmail.com>
2022-09-22 22:41:38 +0530

    first commit

---
+++ prblm1.java
@@ -0,0 +1,15 @@
+/**
+ * prblm1
+ */
+public class prblm1 {
+
+    public static void main(String[] args) {
+        int a = 40;
+        int b= 10;
+
+        System.out.println(a+b);
+        System.out.println("sum of  "+ a + " and "+ b + " is= " + (a+b));
+        System.out.println("Anurag");
+
+    }
+}
